47db6580f026dd127e0a801dadcaac6e711c8ea0
Yordy-code81 <U201923959@upc.edu.pe>
2023-06-02 00:36:42 -0500

    cors added

--- course-service/src/main/java/com/totemcrew/CourseServiceApplication.java
+++ course-service/src/main/java/com/totemcrew/CourseServiceApplication.java
@@ -3,17 +3,28 @@ package com.totemcrew;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableEurekaClient
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseServiceApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
--- director-service/src/main/java/com/totemcrew/DirectorServiceApplication.java
+++ director-service/src/main/java/com/totemcrew/DirectorServiceApplication.java
@@ -3,7 +3,10 @@ package com.totemcrew;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @EnableJpaAuditing
@@ -14,4 +17,14 @@ public class DirectorServiceApplication {
         SpringApplication.run(DirectorServiceApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
