1831489398efe81a7f338c570580d91211690ad1
Yordy-code81 <U201923959@upc.edu.pe>
2023-06-05 21:39:30 -0500

    api gateway fixed

--- course-service/src/main/java/com/totemcrew/CourseServiceApplication.java
+++ course-service/src/main/java/com/totemcrew/CourseServiceApplication.java
@@ -3,6 +3,7 @@ package com.totemcrew;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
