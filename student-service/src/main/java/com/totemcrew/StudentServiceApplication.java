5dbb9ef3273714a6081e226fb772cb65f2a56ca3
Yordy-code81 <U201923959@upc.edu.pe>
2023-06-12 21:34:54 -0500

    feat: student service added

--- student-service/src/main/java/com/totemcrew/StudentServiceApplication.java
+++ student-service/src/main/java/com/totemcrew/StudentServiceApplication.java
@@ -3,28 +3,17 @@ package com.totemcrew;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
 	}
 
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
 }
