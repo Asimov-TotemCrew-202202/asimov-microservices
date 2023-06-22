30a4d96af3c09ef6d56117f25bd02d1cba7abfe9
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-06-21 22:27:48 -0500

    feature: register massive users

--- spring-security-jwt-authentication/src/main/java/com/totemcrew/controllers/AuthController.java
+++ spring-security-jwt-authentication/src/main/java/com/totemcrew/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.totemcrew.controllers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,18 +13,16 @@ import javax.validation.Valid;
 import com.totemcrew.feign.StudentFeignClient;
 import com.totemcrew.payload.request.CreateStudentResource;
 import com.totemcrew.payload.request.SignUpRequestGeneral;
+import com.totemcrew.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.totemcrew.models.ERole;
 import com.totemcrew.models.Role;
@@ -34,6 +35,7 @@ import com.totemcrew.repository.RoleRepository;
 import com.totemcrew.repository.UserRepository;
 import com.totemcrew.security.jwt.JwtUtils;
 import com.totemcrew.security.services.UserDetailsImpl;
+import org.springframework.web.multipart.MultipartFile;
 
 //@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -53,13 +55,6 @@ public class AuthController {
 
   @Autowired
   JwtUtils jwtUtils;
-
-  @Autowired
-  StudentFeignClient studentFeignClient;
-
-  boolean isStudent = false;
-  boolean isTeacher = false;
-  boolean isPrincipal = false;
 
   @PostMapping("/signin")
   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
@@ -160,4 +155,44 @@ public class AuthController {
 
     return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
   }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> uploadUsers(@RequestParam("file") MultipartFile file) {
+    try {
+      List<User> users = new ArrayList<>();
+
+      // Leer el archivo CSV y mapear los datos a objetos User
+      BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+      String line;
+      boolean headerLine = true; // Indica si la línea actual es la línea de encabezado
+      while ((line = reader.readLine()) != null) {
+        if (headerLine) {
+          headerLine = false;
+          continue; // Saltar la línea de encabezado
+        }
+
+        String[] fields = line.split(",");
+
+        // Crea un nuevo objeto User y configura los campos correspondientes
+        User user = new User();
+        user.setUsername(fields[0]);
+        user.setEmail(fields[1]);
+        user.setPassword(fields[2]);
+        user.setFirst_name(fields[3]);
+        user.setLast_name(fields[4]);
+        user.setPhone(fields[5]);
+
+        users.add(user);
+      }
+
+      // Guarda los usuarios en la base de datos
+      userService.saveAll(users);
+
+      return ResponseEntity.ok("Usuarios insertados exitosamente.");
+    } catch (Exception e) {
+      System.out.println(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al insertar usuarios.");
+    }
+  }
+
 }
