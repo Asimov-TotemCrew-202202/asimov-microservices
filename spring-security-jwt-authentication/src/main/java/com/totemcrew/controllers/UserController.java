f29066874b320998d6929482093d457308be1985
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-06-24 19:45:02 -0500

    feature get all users, get by id

---
+++ spring-security-jwt-authentication/src/main/java/com/totemcrew/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.totemcrew.controllers;
+
+import com.totemcrew.models.User;
+        import com.totemcrew.repository.UserRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
