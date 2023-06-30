bc844443e143dc0e37d2809284e51b641cb99cbd
Adrian Esqueiros <90793675+AdrianEsqueiros@users.noreply.github.com>
2023-06-29 20:46:24 -0500

    Update StudentFeignClient.java

--- spring-security-jwt-authentication/src/main/java/com/totemcrew/feign/StudentFeignClient.java
+++ spring-security-jwt-authentication/src/main/java/com/totemcrew/feign/StudentFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "student-service", path = "/api/v1/students")
+@FeignClient(name = "student-service", url = "http://13.59.217.223:8030/api/v1/students")
 public interface StudentFeignClient {
 
     @PostMapping()
